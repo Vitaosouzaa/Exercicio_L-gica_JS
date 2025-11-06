e1a37c679695827c39ca11274884c8a53a28d870
Vitor <vitoorsouzaa123@.gmail.com>
2025-11-06 15:36:58 -0300

    Exercícios de Lógica de Programação

---
+++ logica_intermediario2/string/validarSenha.js
@@ -0,0 +1,19 @@
+//Crie uma função que valide se uma senha é forte (mínimo 8 caracteres, pelo menos 1 número, 1 maiúscula e 1 símbolo).
+
+function validarSenha(string, limite) {
+  const senha = string.length >= limite;
+  const numero = "0123456789";
+  const maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const simbolo = "!@#$%^&*(),.?':{}|<>";
+
+  const caracteres = string.split("");
+
+  const temNumero = caracteres.some((c) => numero.includes(c));
+  const temMaiuscula = caracteres.some((c) => maiuscula.includes(c));
+  const temSimbolos = caracteres.some((c) => simbolo.includes(c));
+  const senhaForte = senha && temNumero && temMaiuscula && temSimbolos;
+
+  return { senha, temNumero, temMaiuscula, temSimbolos, senhaForte };
+}
+
+console.log(validarSenha("123asd12d", 8));
